793a07d71f92cb7edd1074631949c830466cd4e4
Jaime García Méndez <jaimegarciamendez@gmail.com>
2023-09-05 10:29:18 -0300

    test heroku url

--- src/components/customers/get_api_customers.jsx
+++ src/components/customers/get_api_customers.jsx
@@ -2,7 +2,7 @@ import apiUrl from "../../api_routes/api_url";
 import axios from "axios";
 
 function getAPICustomers() {
-  return axios.get(apiUrl() + "/customers").then((response) => response.data)
+  return axios.get("https://mundosat-backend-f7c2dccd1049.herokuapp.com/api/v1/customers").then((response) => response.data)
   .catch((error) => {
     console.error("Error fetching data: ", error);
     return [];
--- src/components/services/get_api_services.jsx
+++ src/components/services/get_api_services.jsx
@@ -2,7 +2,7 @@ import apiUrl from "../../api_routes/api_url";
 import axios from "axios";
 
 function getAPIServices() {
-  return axios.get(apiUrl() + "/services").then((response) => response.data)
+  return axios.get("https://mundosat-backend-f7c2dccd1049.herokuapp.com/api/v1/services").then((response) => response.data)
   .catch((error) => {
     console.error("Error fetching data: ", error);
     return [];

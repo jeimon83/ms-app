72b2ebfc744bca7fa1abb8e3e334cd4c11b7aace
Jaime García Méndez <jaimegarciamendez@gmail.com>
2023-09-05 11:08:46 -0300

    env var

--- src/api_routes/api_url.jsx
+++ src/api_routes/api_url.jsx
@@ -2,7 +2,7 @@ function apiUrl() {
   let prod_url = "https://mundosat-backend-f7c2dccd1049.herokuapp.com/api/v1"
   let development_url = "http://localhost:3001/api/v1"
 
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.REACT_ENV === 'production') {
     console.log("prod_url: ", prod_url) 
     return prod_url
   } else {

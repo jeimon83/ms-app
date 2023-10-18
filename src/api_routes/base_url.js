b43a5f79572b1ed42f97de8426ccc8d31c709f4f
Jaime García Méndez <jaimegarciamendez@gmail.com>
2023-10-18 12:29:11 -0300

    url

--- src/api_routes/base_url.js
+++ src/api_routes/base_url.js
@@ -1,6 +1,6 @@
 function baseUrl() { 
-  //return "https://mundosat-backend-f7c2dccd1049.herokuapp.com";
-  return "http://localhost:3000";
+  return "https://mundosat-backend-f7c2dccd1049.herokuapp.com";
+  //return "http://localhost:3000";
 }
 
 export default baseUrl;
